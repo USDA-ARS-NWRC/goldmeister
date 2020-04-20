478149c04be2fb23270a7186b0cc2cd3f2af2512
Micah Johnson <micah.johnson150@gmail.com>
2020-04-20 15:47:01 -0600

    Added logging that ignores matplotlib

--- goldmeister/utilities.py
+++ goldmeister/utilities.py
@@ -1,12 +1,17 @@
 import logging
+import logging.config
 import coloredlogs
 
 def get_logger(name, level='DEBUG'):
     """
     Retrieve the logger with colored logs in it.
     """
-
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': True,
+    })
     fmt = fmt = '%(name)s %(levelname)s %(message)s'
     log = logging.getLogger(name)
     coloredlogs.install(fmt=fmt, level=level.upper(), logger=log)
+
     return log
