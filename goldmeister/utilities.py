6e080f04339e1b32c62cafc6eb8acfdd3940e3e0
Micah Johnson <micah.johnson150@gmail.com>
2020-04-21 12:32:00 -0600

    still working on logging issues with other modules

--- goldmeister/utilities.py
+++ goldmeister/utilities.py
@@ -6,12 +6,17 @@ def get_logger(name, level='DEBUG'):
     """
     Retrieve the logger with colored logs in it.
     """
+    # logging.config.dictConfig({
+    #     'version': 1,
+    #     'disable_existing_loggers': True,
+    # })
     logging.config.dictConfig({
         'version': 1,
         'disable_existing_loggers': True,
     })
-    fmt = fmt = '%(name)s %(levelname)s %(message)s'
-    log = logging.getLogger(name)
+    log = logging.getLogger(__name__)
+
+    fmt = '%(name)s %(levelname)s %(message)s'
     coloredlogs.install(fmt=fmt, level=level.upper(), logger=log)
 
     return log
